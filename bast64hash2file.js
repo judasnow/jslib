d7be71e7979eb2c0dc002b439708456d663fdebf
vincent.s.shi <judasnow@gmail.com>
2019-05-21 11:13:43 +0800

    add hashCode function

--- bast64hash2file.js
+++ bast64hash2file.js
@@ -7,3 +7,11 @@ function dataURLtoFile(dataurl, filename) {
 	}
 	return new File([u8arr], filename, {type:mime})
 }
+
+function hashCode(s) {
+	let h;
+	for (let i = 0; i < s.length; i++) {
+		h = Math.imul(31, h) + s.charCodeAt(i) | 0;
+	}
+	return h;
+}
